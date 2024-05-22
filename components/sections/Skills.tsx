cd09a4e7ba2f0cd3844f997c57ef936bc6f42332
Sougata <sougatadas9874@gmail.com>
2024-05-22 20:11:46 +0530

    skills ui fix.

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
       <h1 className="section-heading">My Skills</h1>
       <p className="section-subheading">Tech Stack</p>
 
-      <div className="mt-4 lg:mt-8 mx-auto pt-10 lg:pt-16 pb-4 lg:py-8 max-w-md flex flex-wrap items-center justify-center gap-6">
+      <div className="mt-2 lg:mt-8 mx-auto pt-10 lg:pt-16 pb-4 lg:py-8 max-w-md flex flex-wrap items-center justify-center gap-6">
         {skillsData.map((skill) => (
           <SkillBadge key={skill.label} label={skill.label} icon={skill.icon} />
         ))}
