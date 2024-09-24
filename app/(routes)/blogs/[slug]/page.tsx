c5b314b1b23aaae0de4563145cc89038749372df
Sougata <sougatadas9874@gmail.com>
2024-09-24 23:40:20 +0530

    remove on this page from blog page.

--- app/(routes)/blogs/[slug]/page.tsx
+++ app/(routes)/blogs/[slug]/page.tsx
@@ -6,7 +6,6 @@ import { formatDate } from "@/lib/utils";
 import { notFound } from "next/navigation";
 
 import Connect from "@/components/Connect";
-import OnThisPage from "@/components/blog/OnThisPage";
 import { MDXContent } from "@/components/mdx-components";
 import BackButton from "@/components/projects/BackButton";
 import PageTransition from "@/components/animations/PageTransition";
@@ -34,31 +33,27 @@ const page = ({ params }: Props) => {
   return (
     <PageTransition>
       <section className="flex flex-col pb-16">
-        <div className="flex flex-col lg:flex-row lg:gap-8">
-          <div>
-            <BackButton href="/blogs" />
-            <div className="flex flex-col gap-4">
-              <div>
-                <h1 className="text-2xl text-light font-semibold">
-                  {post.title}
-                </h1>
-                <p className="secondary-text">
-                  {post.description ? post.description : ""}
-                </p>
-              </div>
-
-              <p className="text-sm text-light/40">{formatDate(post.date)}</p>
+        <div className="flex flex-col">
+          <BackButton href="/blogs" />
+          <div className="flex flex-col gap-4">
+            <div>
+              <h1 className="text-2xl text-light font-semibold">
+                {post.title}
+              </h1>
+              <p className="secondary-text">
+                {post.description ? post.description : ""}
+              </p>
             </div>
 
-            {/* Content */}
-            <MDXContent code={post.body} className="blog-content-styles" />
+            <p className="text-sm text-light/40">{formatDate(post.date)}</p>
           </div>
 
-          <OnThisPage />
-        </div>
+          {/* Content */}
+          <MDXContent code={post.body} className="blog-content-styles" />
 
-        <div className="mt-8">
-          <Connect />
+          <div className="mt-8">
+            <Connect />
+          </div>
         </div>
       </section>
     </PageTransition>
