f5ceebc96b3686125cb1854010ebd03769f7ef79
Sougata <sougatadas9874@gmail.com>
2024-10-26 10:43:53 +0530

    fix: ui of more code component.

--- components/blog/MoreCode.tsx
+++ components/blog/MoreCode.tsx
@@ -1,18 +1,22 @@
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+
 const MoreCode = () => {
   return (
     <div className="flex flex-col gap-1 max-w-[420px] md:max-w-[480px]">
-      <h2 id="code" className="heading-text">
-        Code
-      </h2>
-      <p className="secondary-text">
-        <a
-          target="_blank"
-          className="underline"
+      <div className="">
+        <Link
           href="https://github.com/sougata-github/TypeScript"
+          target="_blank"
+          className="flex gap-1 items-center font-semibold text-xl text-light mt-8 mb-2"
         >
-          TypeScript
-        </a>{" "}
-        - feel free to browse the repository, raise issues, share your feedback,
+          Code
+          <ArrowUpRight className="text-light size-4" />
+        </Link>
+      </div>
+
+      <p className="secondary-text m-0">
+        Feel free to browse the repository, raise issues, share your feedback,
         and contribute to the project.
       </p>
     </div>
